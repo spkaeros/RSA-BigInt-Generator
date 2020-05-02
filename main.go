dc3b82d3b497f7c6825a98bcff161452d09bddd3
Zach Knight <zach.knight@protonmail.com>
2020-05-02 04:09:37 -0500

    cleanup

--- main.go
+++ main.go
@@ -8,54 +8,59 @@ import (
 	"math/big"
 )
 
-var pub = big.NewInt(65537)
 var one = big.NewInt(1)
-var mod = new(big.Int)
-var priv = new(big.Int)
-//var pub = new(big.Int)
+var tmp = new(big.Int)
 
-func init() {
-	bitSize := 512
-	if i, err := strconv.Atoi(os.Args[1]); err == nil && i <= 4096 && i >= 512 {
-		bitSize = i
-	}
-	tmp, err := rand.Prime(rand.Reader, bitSize)
+const minBits = 512
+const maxBits = 16384
+
+func checkErr(bi *big.Int, err error) *big.Int {
 	if err != nil {
-		fmt.Println("something bad happened")
-		os.Exit(100)
+		fmt.Println("Something bad happened during prime number generation:", err.Error())
+		return nil
 	}
-	tmp2, err := rand.Prime(rand.Reader, bitSize)
-	if err != nil {
-		fmt.Println("something bad happened")
-		os.Exit(100)
-	}
-	p, err := rand.Prime(rand.Reader, bitSize)
-	if err != nil {
-		fmt.Println("something bad happened")
-		os.Exit(100)
-	}
-	pub.Set(p)
-	fmt.Println("pub:", pub)
-	mod.Mul(tmp, tmp2)
-	fmt.Println("mod:", mod)
-	phi := new(big.Int)
-	phi.Mul(tmp.Sub(tmp, one), tmp2.Sub(tmp2, one)) //.Mul(tmp2.Sub(one)) 
-	fmt.Println("priv:", priv.ModInverse(pub,phi))
+
+	return bi
 }
 
 func main() {
-	fmt.Printf("Testing new RSA keys...")
-	b := big.NewInt(500)
-	before := b.Exp(b, pub, mod)
-	after := b.Exp(b, priv, mod)//, //b.ModExp(priv, mod), b)
-	if before.String() == "500" && after.String() == "500" {
-		fmt.Printf("Test passed!  You're good to go, place mod and pub in the client.")
-	} else {
-		fmt.Println("Test failed!  Something went wrong, before and after encryption and decryption do not match the control var")
+	bitSize := minBits
+	if len(os.Args) > 1 {
+		if i, err := strconv.Atoi(os.Args[1]); err == nil && i <= maxBits && i >= minBits {
+			bitSize = i
+		}
 	}
-//	fmt.Println(big.NewInt(500).ModExp(pub, mod))
-//	random, _ := rand.Prime(rand.Reader, 4096)
-//	fmt.Println(b.Exp(65537), b.Exp(65537))
-//	fmt.Println(b.Exp(65537))
-//	for 
+	var q = checkErr(rand.Prime(rand.Reader, bitSize))
+	var d = checkErr(rand.Prime(rand.Reader, bitSize))
+	fmt.Printf("q:%v\nd:%v\n\n", q, d)
+	fmt.Println("Deriving RSA exponents/modulus from q and d...")
+
+	var pub *big.Int
+	if len(os.Args) > 2 && os.Args[2] != "random" {
+		if i, err := strconv.Atoi(os.Args[2]); err == nil {
+			if pubExp := big.NewInt(int64(i)); pubExp.ProbablyPrime(bitSize) {
+				pub = pubExp
+			}
+		}
+	}
+	if pub == nil {
+		pub = checkErr(rand.Prime(rand.Reader, bitSize))
+	}
+	
+	mod := new(big.Int).Mul(q, d)
+	priv := new(big.Int).ModInverse(pub, tmp.Mul(q.Sub(q, one), d.Sub(d, one)))
+	fmt.Println("publicExponent:", pub)
+	fmt.Println("privateExponent:", priv)
+	fmt.Println("modulus:", mod)
+
+	fmt.Printf("Testing new RSA keys...")
+	control := big.NewInt(500)
+	encrypted := tmp.Exp(control, pub, mod)
+	decrypted := tmp.Exp(encrypted, priv, mod)
+
+	if control.String() == decrypted.String() {
+		fmt.Printf("Test passed!\nThe generated primes work for RSA encryption.\nPlace the modulus and and the publicExponent in the client, and place the modulus and the privateExponent in the server, and everything should work.\n")
+	} else {
+		fmt.Printf("Test failed!\nSomething went wrong, before and after encryption did not match the control variable.\n")
+	}
 }
