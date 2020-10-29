5ba5f467f4d12a7098ab24614c3dd60a183f70dd
Zach Knight <zach.knight@pm.me>
2020-10-29 11:17:34 -0500

    Add concurrency in areas it makes sense for

--- main.go
+++ main.go
@@ -9,9 +9,8 @@ import (
 )
 
 var one = big.NewInt(1)
-var tmp = new(big.Int)
 
-const minBits = 512
+const minBits = 256
 const maxBits = 16384
 
 func checkErr(bi *big.Int, err error) *big.Int {
@@ -30,37 +29,49 @@ func main() {
 			bitSize = i
 		}
 	}
-	var q = checkErr(rand.Prime(rand.Reader, bitSize))
-	var d = checkErr(rand.Prime(rand.Reader, bitSize))
+	primeChan := make(chan *big.Int)
+	fmt.Println("Finding some random " + strconv.Itoa(bitSize) + "-bit long prime numbers for use as q and d")
+	for i := 0; i < 2; i++ {
+		go func() {
+			primeChan <- checkErr(rand.Prime(rand.Reader, bitSize))
+		}()
+	}
+	q, d := <-primeChan, <-primeChan
 	fmt.Printf("q:%v\nd:%v\n\n", q, d)
-	fmt.Println("Deriving RSA exponents/modulus from q and d...")
+	fmt.Println("Doing math on q and d to arrive at RSA exponents/modulus...")
 
 	var pub *big.Int
-	if len(os.Args) > 2 && os.Args[2] != "random" {
-		if i, err := strconv.Atoi(os.Args[2]); err == nil {
-			if pubExp := big.NewInt(int64(i)); pubExp.ProbablyPrime(bitSize) {
-				pub = pubExp
+	go func() {
+		var pubExp *big.Int
+		if len(os.Args) > 2 && os.Args[2] != "random" {
+			if i, err := strconv.Atoi(os.Args[2]); err == nil {
+				pubExp = big.NewInt(int64(i))
 			}
 		}
-	}
-	if pub == nil {
-		pub = checkErr(rand.Prime(rand.Reader, bitSize))
-	}
+		if pubExp == nil || !pubExp.ProbablyPrime(bitSize) {
+			pubExp = checkErr(rand.Prime(rand.Reader, bitSize))
+		}
+		primeChan <- pubExp
+	}()
+	pub = <-primeChan
+	var priv, mod *big.Int
+	go func() {
+		primeChan <- new(big.Int).ModInverse(pub, new(big.Int).Mul(q.Sub(q, one), d.Sub(d, one)))
+	}()
 	
-	mod := new(big.Int).Mul(q, d)
-	priv := new(big.Int).ModInverse(pub, tmp.Mul(q.Sub(q, one), d.Sub(d, one)))
-	fmt.Println("publicExponent:", pub)
-	fmt.Println("privateExponent:", priv)
-	fmt.Println("modulus:", mod)
+	mod = new(big.Int).Mul(q, d)
+	priv = <-primeChan
+	fmt.Printf("modulus:%d\n\npublic:%d\n\nprivate:%d\n\n\n", mod, pub, priv)
 
-	fmt.Printf("Testing new RSA keys...")
+	fmt.Printf("Testing new keys...")
 	control := big.NewInt(500)
-	encrypted := tmp.Exp(control, pub, mod)
-	decrypted := tmp.Exp(encrypted, priv, mod)
+	encrypted := new(big.Int).Exp(control, pub, mod)
+	decrypted := new(big.Int).Exp(encrypted, priv, mod)
 
 	if control.String() == decrypted.String() {
-		fmt.Printf("Test passed!\nThe generated primes work for RSA encryption.\nPlace the modulus and and the publicExponent in the client, and place the modulus and the privateExponent in the server, and everything should work.\n")
+		fmt.Printf("Test passed!\nThe generated primes work for RSA encryption.\nPlace the modulus and and the public exponent in the client, and place the modulus and the private exponent in the server, and everything should work.\n")
 	} else {
 		fmt.Printf("Test failed!\nSomething went wrong, before and after encryption did not match the control variable.\n")
+		fmt.Println(control, decrypted, encrypted)
 	}
 }
